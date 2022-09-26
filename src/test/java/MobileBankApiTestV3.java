2c51c24cf65c76bb0a3d2d7c73fb353b30f1d09d
Егор Старшелюк <granegoro@gmail.com>
2022-09-26 18:25:40 +0300

    test2 error

--- src/test/java/MobileBankApiTestV3.java
+++ src/test/java/MobileBankApiTestV3.java
@@ -1,18 +1,27 @@
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
-class MobileBankApiTestV2 {
+class MobileBankApiTestV3 {
     @Test
     void shouldReturnDemoAccounts() {
+
         given()
                 .baseUri("http://localhost:9999/api/v1")
+
                 .when()
                 .get("/demo/accounts")
+
                 .then()
-                .statusCode(300)
+                .statusCode(200)
+
                 .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[0].currency", equalTo("RUB"))
+                .body("[0].balance", greaterThanOrEqualTo(0))
         ;
     }
 }
